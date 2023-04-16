b6089aa1542375e34a52e372f7faf1cdf240389a
Mandy Delmaine <mandydelmaine@gmail.com>
2023-04-16 10:51:58 -0400

    updated functions to get employee data

--- query.js
+++ query.js
@@ -17,11 +17,24 @@ const getAllDepartments = async () => {
 }
 
 const getAllRoles = async () => {
-    const data = await db.promise().execute("SELECT * FROM `role`");
+    const data = await db.promise().execute(
+        "SELECT `role`.`id`, `role`.`title`, `department`.`name` AS `department`, `role`.`salary`\
+         FROM `role`\
+         JOIN `department`\
+         ON `role`.`department_id` = `department`.`id`");
     return data[0];
 }
 const getAllEmployees = async () => {
-    const data = await db.promise().execute("SELECT * FROM `employee`");
+    const data = await db.promise().execute(
+        "SELECT `e`.`id`, `e`.`first_name`, `e`.`last_name`, `r`.`title`, `d`.`name` AS `department`, `r`.`salary`, CONCAT(`m`.`first_name`, ' ', `m`.`last_name`) AS `manager`\
+         FROM `employee` e\
+         JOIN `role` r\
+         ON `r`.`id` = `e`.`role_id`\
+         JOIN `department` d\
+         ON  `r`.`department_id` = `d`.`id`\
+         LEFT JOIN `employee` m\
+         ON `e`.`manager_id` = `m`.`id`\
+         ORDER BY `e`.`id` ASC");
     return data[0];
 }
 
